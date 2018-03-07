99f4e32885c9249704b32704b44f08744bbace6c
Ricardo Bittencourt <bluepenguin@gmail.com>
2018-03-07 10:52:52 -0300

    Added more nodes

--- handshake.sh
+++ handshake.sh
@@ -1,1 +1,2 @@
-cat handshake.dot | dijkstra ricbit | gvpr -c "N{label=sprintf(\"%s (%.0f)\",label,dist)}" | dot -Tpng -o handshake.png
+cat handshake.dot | dijkstra ricbit | gvpr -c "N{label=sprintf(\"%s (%.0f)\",label,dist)}" | dot -Tpng | convert -quality 95 - handshake.png
+
